3cf8fb5b29effe86ea86705d8d0f5079b5afa853
slowik <aleksandra.slowik88@gmail.com>
2020-09-23 15:13:01 +0200

    add bootstrap cards

--- wp-content/themes/nevida/index.php
+++ wp-content/themes/nevida/index.php
@@ -1,16 +1,16 @@
 <?php get_header() ?>
 
 <?php if (have_posts()): ?>
-    <ul>
     <?php while(have_posts()): the_post(); ?>
-    <li>
-        <a href="<?php the_permalink() ?>">
-        <?php the_title() ?>
-        </a>    
-        -
-        <?php the_author() ?></li>
-    <?php endwhile ?>
-    </ul>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><?php the_title() ?></h5>
+    <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+    <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo et enim asperiores. Eius, odit? Dignissimos dolores numquam inventore odit? Deleniti molestiae laboriosam inventore quod dolorum fuga nemo exercitationem? Quaerat, asperiores.</p>
+    <a href="#" class="card-link"><?php the_permalink() ?></a>
+    <a href="#" class="card-link"><?php the_author() ?></a>
+  </div>
+    <?php endwhile ?>=
 <?php else: ?>
     <h1>Aucun article</h1>
 <?php endif; ?>
