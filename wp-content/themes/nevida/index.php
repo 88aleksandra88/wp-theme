6e1de65f162f4aca76734ff83da034f689055cb4
slowik <aleksandra.slowik88@gmail.com>
2020-09-23 17:23:48 +0200

    singel-post.php done

--- wp-content/themes/nevida/index.php
+++ wp-content/themes/nevida/index.php
@@ -11,9 +11,9 @@
                 <h5 class="card-title"><?php the_title() ?></h5>
                 <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
                 <p class="card-text">
-                    <?php the_content('voir plus...') ?>
+                    <?php the_content() ?>
                 </p>
-                <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                
                 <p class="card-text"><?php the_author() ?></p>
             </div>
         </div>
--- wp-content/themes/nevida/single-post.php
+++ wp-content/themes/nevida/single-post.php
@@ -1,29 +1,11 @@
 <?php get_header() ?>
 
 <?php if (have_posts()) : ?>
-<div class="row">
 
-    <?php while (have_posts()) : the_post(); ?>
-    <div class="col-sm-4">
-        <div class="card">
-            <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto;']) ?>
-            <div class="card-body">
-                <h5 class="card-title"><?php the_title() ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
-                <p class="card-text">
-                    <?php the_content('voir plus...') ?>
-                </p>
-                <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
-                <p class="card-text"><?php the_author() ?></p>
-            </div>
-        </div>
-    </div>
-    <?php endwhile ?>
-
-</div>
-
-<?php else : ?>
-<h1>Aucun article</h1>
-<?php endif; ?>
+<?php while (have_posts()) : the_post(); ?>
+    <h1><?php the_title() ?></h1>
+    <?php the_post_thumbnail() ?>
+    <?php the_content() ?>
+<?php endwhile; endif; ?>
 
 <?php get_footer() ?>
