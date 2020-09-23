833e459a2f8105147dbb42405b3eff077654c8f3
slowik <aleksandra.slowik88@gmail.com>
2020-09-23 14:43:07 +0200

    add articles, permalinks, author index.php

--- wp-content/themes/nevida/index.php
+++ wp-content/themes/nevida/index.php
@@ -3,7 +3,12 @@
 <?php if (have_posts()): ?>
     <ul>
     <?php while(have_posts()): the_post(); ?>
-    <li><?php the_title() ?></li>
+    <li>
+        <a href="<?php the_permalink() ?>">
+        <?php the_title() ?>
+        </a>    
+        -
+        <?php the_author() ?></li>
     <?php endwhile ?>
     </ul>
 <?php else: ?>
