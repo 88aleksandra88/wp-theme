0deb1e5f3fcde32958a3821d5ac4b40c53913c40
slowik <aleksandra.slowik88@gmail.com>
2020-09-23 16:21:10 +0200

    cards text update format

--- wp-content/themes/nevida/index.php
+++ wp-content/themes/nevida/index.php
@@ -1,19 +1,27 @@
 <?php get_header() ?>
 
-<?php if (have_posts()): ?>
-    <?php while(have_posts()): the_post(); ?>
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-    <h5 class="card-title"><?php the_title() ?></h5>
-    <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
-    <p class="card-text">
-        <?php the_content('voir plus...') ?>
-    </p>
-    <a href="<?php the_permalink() ?>"class="card-link">Voir plus</a>
-    <p class="card-text"><?php the_author() ?></p>
-  </div>
-    <?php endwhile ?>
-<?php else: ?>
+<?php if (have_posts()) : ?>
+    <div class="row">
+
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text">
+                            <?php the_content('voir plus...') ?>
+                        </p>
+                        <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                        <p class="card-text"><?php the_author() ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile ?>
+
+    </div>
+
+<?php else : ?>
     <h1>Aucun article</h1>
 <?php endif; ?>
 
